b0a7a4671e24aa7089e2f6f5119ca161211d23e3
Vitaliy <itvkip@yandex.ru>
2021-01-04 11:10:27 +0300

    - added hook useState for input;

--- src/Components/listItem.js
+++ src/Components/listItem.js
@@ -1,8 +1,17 @@
+import React, { useState } from 'react';
+
 function ListItem(props){
+	const [enterAnswer, setAnswer] = useState('');
+
 	const id_question = 'quest-' + props.questId;
 	return <div>
 		<label htmlFor={ id_question }>{ props.title }</label>
-		<input type='text' id={ id_question }/>
+		<input 
+			type='text'
+			id={ id_question }
+			value={ enterAnswer }
+			onChange={(event) => setAnswer(event.target.value)}
+		/>
 		<p>{ props.answer }</p>
 	</div>
 }
