dbef718e1b02e46deb3a9a9630acf6bc32484efb
Vitaliy <itvkip@yandex.ru>
2021-01-04 10:56:15 +0300

    - after init loading page, displaying only first question; - added input after question for entring right answer;

--- src/Components/Common.js
+++ src/Components/Common.js
@@ -1,12 +1,14 @@
 import ListItem from './listItem.js';
 
+import style from './common.module.css';
+
 function Common(props){
 	const quest = props.questions;
 	const listItems = quest.map((item) => 
-		<ListItem key={ item.id_quest } title={ item.name_quest } answer = { item.answer }/>
+		<ListItem key={ item.id_quest } title={ item.name_quest } answer={ item.answer } questId={ item.id_quest }/>
 	);
 	return (
-		<div>
+		<div className={ style.common }>
 			{ listItems }
 		</div>
 	);
--- src/Components/listItem.js
+++ src/Components/listItem.js
@@ -1,6 +1,8 @@
 function ListItem(props){
+	const id_question = 'quest-' + props.questId;
 	return <div>
-		<p>{ props.title }</p>
+		<label htmlFor={ id_question }>{ props.title }</label>
+		<input type='text' id={ id_question }/>
 		<p>{ props.answer }</p>
 	</div>
 }
