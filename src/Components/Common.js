678cfdc59167c953fb6eac7dbf7f428d809ae038
Vitaliy <itvkip@yandex.ru>
2021-01-08 12:18:20 +0300

    - questions change after click on button "Move to next question"

--- src/Components/Common.js
+++ src/Components/Common.js
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import ListItem from './listItem.js';
 import style from './common.module.css';
 
 function Common(props){
 	const quest = props.questions;
-	const listItems = quest.map((item) => 
+	const [startInd, setStartInd] = useState(0);
+
+	const changeQuest = (val) => {
+		setStartInd(val);
+	}
+
+	const listItems = quest.map((item, index) => 
+		index === startInd ?
 		<ListItem 
+			dataInd={ index }
 			key={ item.id_quest }
 			title={ item.name_quest }
 			answers={ item.answers }
 			rightAns={ item.rightAnswer }
-			idQuest={ item.id_quest }/>
+			idQuest={ item.id_quest }
+			changeItem={ changeQuest }/>
+		: ''
 	);
 	return (
 		<div className={ style.common }>
--- src/Components/listItem.js
+++ src/Components/listItem.js
@@ -2,12 +2,12 @@ import ItemAnswer from './ItemAnswer.js';
 import React, { useState } from 'react';
 
 function ListItem(props){
+	let mes;
 	const answers = props.answers;
 
 	const [answerRight, isAnswerRight] = useState('');
 
-	const testFunc = (val) => {
-		console.log(val)
+	const checkAnswer = (val) => {
 		if (val === props.rightAns){
 			isAnswerRight(true);
 			return false;
@@ -15,13 +15,18 @@ function ListItem(props){
 		isAnswerRight(false);
 	}
 
-	let mes;
+	const nextQuest = () => {
+		const nextInd = props.dataInd + 1;
+		console.log(nextInd);
+		props.changeItem(nextInd);
+	}
+
 	switch(answerRight){
 		case true:
-			mes = <React.Fragment><p>Верно</p><button type='button'>Следующий вопрос</button></React.Fragment>;
+			mes = <React.Fragment><p>Верно</p><button type='button' onClick={ nextQuest }>Следующий вопрос</button></React.Fragment>;
 			break;
 		case false:
-			mes = <React.Fragment><p>Не верно</p><button type='button'>Следующий вопрос</button></React.Fragment>;
+			mes = <React.Fragment><p>Не верно</p><button type='button' onClick={ nextQuest }>Следующий вопрос</button></React.Fragment>;
 			break;
 		default:
 			mes = '';
@@ -34,10 +39,10 @@ function ListItem(props){
 			val={ answer.answer }
 			idQuest={ props.idQuest }
 			idAnsw={ answer.id }
-			func={ testFunc }/>
+			func={ checkAnswer }/>
 	);
 
-	return <div>
+	return <div data-ind={ props.dataInd }>
 		<form>
 			{ listAnswer }
 			{ mes }
