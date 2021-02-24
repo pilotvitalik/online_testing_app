3f03fe4943ac9dbc873c4cfc67f5c44e08013fb7
Vitaliy <itvkip@yandex.ru>
2021-02-24 17:13:59 +0300

    Revert "- added function for mixing answer in question;"
    This reverts commit 44c85fbefb7791884c757085198869547fe3d8b5.

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -10,22 +10,8 @@ class Test extends Component{
 		super(props);
 		this.state = {
 			quest: this.props.questions,
-			answers_arr: '',
 		}
 		this.defStatus = this.defStatus.bind(this);
-		this.mixAnswer = this.mixAnswer.bind(this);
-	}
-
-	mixAnswer(arr){
-		console.log('mix answer');
-		console.log(arr);
-		for (let i = arr.length - 1; i > 0; i--) {
-		    let j = Math.floor(Math.random() * (i + 1));
-		    [arr[i], arr[j]] = [arr[j], arr[i]];
-		  }
-		 this.setState({
-		 	answers_arr: arr,
-		 });
 	}
 
 	componentDidMount(){
@@ -48,29 +34,25 @@ class Test extends Component{
 		});
 	}
 
-	
-
 	render(){
 		let displayEl;
 		const testDone = <h2>Тест пройден</h2>;
-		const arrQuest = this.state.quest; // edit
-		const listItems = arrQuest.map((item, index) => {
-			if (index === this.props.startInd) {
-				//this.mixAnswer(item.answers);
-				console.log(this.state.answers_arr)
-				return <ListItem 
-					dataInd={ index }
-					question={item.name_quest}
-					key={ item.id_quest }
-					title={ item.name_quest }
-					answers={ item.answers }
-					rightAns={ item.rightAnswer }
-					idQuest={ item.id_quest }
-					changeItem={ this.props.changeItem }
-					statusAnswer={ this.defStatus }
-					titleQuest={ item.vis_name_test }/>
-			}
-		});
+		const arrQuest = this.state.quest;
+		const listItems = arrQuest.map((item, index) => 
+			index === this.props.startInd ?
+			<ListItem 
+				dataInd={ index }
+				question={item.name_quest}
+				key={ item.id_quest }
+				title={ item.name_quest }
+				answers={ item.answers }
+				rightAns={ item.rightAnswer }
+				idQuest={ item.id_quest }
+				changeItem={ this.props.changeItem }
+				statusAnswer={ this.defStatus }
+				titleQuest={ item.vis_name_test }/>
+			: ''
+		);
 
 		this.props.startInd !== this.state.quest.length ? displayEl = listItems : displayEl = testDone;
 
